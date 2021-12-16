2db48d7a137a3143348a0e95e8db9c428467d419
Bobster82 <47780758+Bobster82@users.noreply.github.com>
2021-12-16 13:34:16 +0100

    Changed default tab keys

--- VInfo.lua
+++ VInfo.lua
@@ -42,7 +42,7 @@ VInfo = {};
 VInfo.name = "VInfo";
 VInfo.fullName = "Vehicle Info";
 VInfo.author = "Bobster82";
-VInfo.version = "1.0.0.1";
+VInfo.version = "1.0.0.2";
 VInfo.dir = g_currentModDirectory;
 VInfo.hudName = "VINFO_HUD";
 
